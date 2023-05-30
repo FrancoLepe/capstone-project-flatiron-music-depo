44eda08b08e6617edf38a30d0f9ac673ff9d681b
Franco Lepe <Francolepe169@gmail.com>
2023-05-30 15:20:42 -0500

    removed cart id from history

--- client/src/Components/PurchaseHistory.js
+++ client/src/Components/PurchaseHistory.js
@@ -72,7 +72,7 @@ const PurchaseHistory = () => {
           <th>ID</th>
           <th>Product ID</th>
           <th>Customer ID</th>
-          <th>Cart ID</th>
+          
           <th>Product Name</th>
           <th>Price</th>
           <th>Purchase Date</th>
@@ -84,7 +84,6 @@ const PurchaseHistory = () => {
           <tr key={history.id}>
             <td>{history.id}</td>
             <td>{history.product_id}</td>
-            <td>{history.customer_id}</td>
             <td>{history.customer_id}</td>
             <td>{history.name}</td>
             <td>{history.price}</td>
